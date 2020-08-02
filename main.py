04a6b8a799fd619606a705a3428590f667808fee
Dominik Baran <dominik.baran1992@gmail.com>
2020-08-02 17:24:23 +0200

    Create hompage template

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_tempalte
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hompage():
-    return render_tempalte("index.html")
+    movies=[]
+    for i in range(10):
+        movies.append(i)
+    return render_template("hompage.html", movies=movies)
+
 
 if __name__ == "__main__":
+    app.config['ENV'] = 'development'
     app.run(debug=True)
